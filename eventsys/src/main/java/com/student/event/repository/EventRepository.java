cc91d029e9f203d0df9be56ff1406cd661b68bb6
tanmayeerela <tanmayerela@gmail.com>
2026-04-01 20:15:08 +0530

    Delete eventsys directory

--- eventsys/src/main/java/com/student/event/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.student.event.repository;
-
-import com.student.event.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- eventsys/src/main/java/com/student/event/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.event.repository;
-
-import com.student.event.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
